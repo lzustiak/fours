75d97206aa82a2eb838956de0a631726d3b5a95e
Levi Zustiak <91484241+lzustiak@users.noreply.github.com>
2023-02-27 19:43:39 -0600

    [feat] add basic navigation to Home page

--- resources/ts/Pages/Home.tsx
+++ resources/ts/Pages/Home.tsx
@@ -1,3 +1,20 @@
-export default function Home(): JSX.Element {
-  return <div>Home</div>;
+import { User } from "@/types/user.types";
+import { Link } from "@inertiajs/react";
+
+type Props = {
+  user: User;
+};
+
+export default function Home({ user }: Props): JSX.Element {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/logout">Logout</Link>
+      <Link href="/login">Login</Link>
+      <Link href="/register">Register</Link>
+      <Link href="/lobby/create">Create</Link>
+      <Link href="/lobby/join">Join</Link>
+      <pre>{JSON.stringify(user, null, 4)}</pre>
+    </div>
+  );
 }

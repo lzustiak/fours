bd20dee6dd110b1ba9c0453cc5dbe4edf2d5a024
Levi Zustiak <91484241+lzustiak@users.noreply.github.com>
2023-02-27 19:43:17 -0600

    [feat] add Create and Join pages

--- resources/ts/Pages/Lobby/Create.tsx
+++ resources/ts/Pages/Lobby/Create.tsx
@@ -1,3 +1,29 @@
-export default function Create(): JSX.Element {
-  return <div>Create</div>
+import { Lobby } from "@/types/lobby.types";
+import { Link } from "@inertiajs/react";
+
+type Props = {
+  lobby: Lobby;
+};
+
+export default function Create({ lobby }: Props): JSX.Element {
+  const copyLink = () =>
+    navigator.clipboard.writeText(
+      `${import.meta.env.VITE_APP_URL || ""}/lobby/join/${lobby.id}`,
+    );
+
+  return (
+    <div>
+      <h1>Create</h1>
+      <pre>{JSON.stringify(lobby, null, 4)}</pre>
+      <Link
+        href={`/lobby/delete/${lobby.id}`}
+        method="delete"
+        as="button"
+        type="button"
+      >
+        Cancel
+      </Link>
+      <button onClick={copyLink}>Copy Invite</button>
+    </div>
+  );
 }
